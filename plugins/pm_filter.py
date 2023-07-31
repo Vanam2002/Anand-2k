2b77184d7c05deddbf267882f8cec2be3321091f
Vanam2002 <117515657+Vanam2002@users.noreply.github.com>
2023-07-31 18:00:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -62,9 +62,7 @@ async def pm_text(bot, message):
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
     await message.reply_text("<b>ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴀɴᴅ sᴇᴀʀᴄʜ ᴍᴏᴠɪᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ
- 👉🏻 http://telegram.me/+xqfxWxoePGs4ZWJl 👈🏻
-
-ᴀᴅᴍɪɴ - http://telegram.me/Anand_2K 💕</b>")
+ 👉🏻 http://telegram.me/+xqfxWxoePGs4ZWJl</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
